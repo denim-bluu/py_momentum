864f00ec1dc8fa6bfe5ab602b32dbf771741e8d4
Joon Kang <denim.bluu@gmail.com>
2024-07-29 09:40:36 +0400

    chore: Extra emojis

--- py_momentum/app/data/repository/yahoo_finance.py
+++ py_momentum/app/data/repository/yahoo_finance.py
@@ -21,13 +21,13 @@ class YahooFinanceRepository(BaseDataRepository):
         cached_data = get_cache(cache_key)
 
         if cached_data:
-            logger.info(f"Cache hit for {cache_key}")
+            logger.info(f"🧽 Cache hit for {cache_key}")
             return [
                 StockDataWithIndicators.model_validate_json(item)
                 for item in json.loads(cached_data)  # type: ignore
             ]
 
-        logger.info(f"Cache miss for {cache_key}, fetching from database")
+        logger.info(f"💿 Cache miss for {cache_key}, fetching from database")
         db_data = (
             db.query(StockDataDB)
             .filter(
@@ -42,7 +42,7 @@ class YahooFinanceRepository(BaseDataRepository):
             result = [self._db_to_model(item) for item in db_data]
         else:
             logger.info(
-                f"Data not found in database for {symbol}, fetching from Yahoo Finance"
+                f"🛜 Data not found in database for {symbol}, fetching from Yahoo Finance"
             )
             result = await self._fetch_from_yahoo(symbol, start_date, end_date, db)
 
@@ -64,7 +64,7 @@ class YahooFinanceRepository(BaseDataRepository):
             result = []
             for index, row in df.iterrows():
                 stock_data = StockDataWithIndicators(
-                    date=index, # type: ignore
+                    date=index,  # type: ignore
                     open=row["Open"],
                     high=row["High"],
                     low=row["Low"],
